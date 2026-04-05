0cdd2d2664508cfacd264fb5e30ca27cb5275fae
iceysteel <zainm211@hotmail.com>
2026-04-05 06:55:16 -0700

    experiment: threshold 0.45 + dedup radius 8m

--- autoresearch/pipeline.py
+++ autoresearch/pipeline.py
@@ -30,7 +30,7 @@ WMTS_DIR = os.path.join(DATA_DIR, 'wmts')
 # Detection
 DETECTOR = 'sam3'  # 'sam3' or 'sam3_lora_v2'
 SAM3_PROMPT = 'telephone pole'
-SAM3_THRESHOLD = 0.40
+SAM3_THRESHOLD = 0.45
 SAM3_CKPT = os.path.join(os.path.expanduser("~"),
     ".cache/huggingface/hub/models--bodhicitta--sam3/snapshots/"
     "cba430d22f6fdc3f06ad3841274ec7bb55885f2f/sam3.pt")
