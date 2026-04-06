2730ef7944b3e444e73594513085b4227d1fd6b8
iceysteel <zainm211@hotmail.com>
2026-04-05 22:02:31 -0700

    experiment: add generic "pole" prompt at thresh 0.70

--- autoresearch/pipeline.py
+++ autoresearch/pipeline.py
@@ -30,7 +30,7 @@ WMTS_DIR = os.path.join(DATA_DIR, 'wmts')
 # Detection
 DETECTOR = 'sam3'  # 'sam3' or 'sam3_lora_v2'
 SAM3_PROMPT = 'telephone pole'
-SAM3_PROMPTS_EXTRA = [('wooden pole', 0.40), ('power pole', 0.65)]  # (prompt, threshold)
+SAM3_PROMPTS_EXTRA = [('wooden pole', 0.40), ('power pole', 0.65), ('pole', 0.70)]  # (prompt, threshold)
 SAM3_THRESHOLD = 0.40
 SAM3_CKPT = os.path.join(os.path.expanduser("~"),
     ".cache/huggingface/hub/models--bodhicitta--sam3/snapshots/"
